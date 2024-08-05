69842eba38c30548b66a5737b5d9014b979e43d3
Nadeem <itspingo@yahoo.com>
2024-08-05 19:09:05 +0500

    delete card was not working

--- resources/views/frontend/my_cards.blade.php
+++ resources/views/frontend/my_cards.blade.php
@@ -160,7 +160,7 @@
                                         </font>
                                     </span></span></div> -->
 
-                            <div class="option"><a href="{{url('card_delete/'.$mycard->id)}}"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" fill="currentColor" width="24" height="24" viewBox="0 0 24 24" class="icon">
+                            <div class="option"><a href="{{url('mycards/card_delete/'.$mycard->id)}}"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" fill="currentColor" width="24" height="24" viewBox="0 0 24 24" class="icon">
                                     <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z">
                                     </path>
                                 </svg><span><span wudooh="true" style="font-size:1.05em;line-height:1.1em;font-family:&quot;Sahl Naskh&quot;;">
